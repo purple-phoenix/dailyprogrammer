a49b74737867771eef112baefc862a0ae8635a1c
Matt McCarthy <mtmccarthy@wpi.edu>
2019-10-29 21:32:56 -0400

    Fix test should not change the value

--- python/375-card-flipping/card_flipper_tests.py
+++ python/375-card-flipping/card_flipper_tests.py
@@ -71,7 +71,7 @@ class TestCardFlipper(unittest.TestCase):
         self.assertFalse(should_flip_neighbor(0, 2))
 
     def test_flip_neighbor(self):
-        self.assertEquals(None, flip_neighbor(0, 2, True))
+        self.assertEquals(True, flip_neighbor(0, 2, True))
         self.assertEquals(None, flip_neighbor(0, 1, None))
         self.assertEquals(True, flip_neighbor(0, 1, False))
         self.assertEquals(False, flip_neighbor(1, 2, True))
