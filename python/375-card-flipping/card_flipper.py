7255d591472afae101fe06a14b31baa23b87626f
Matt McCarthy <mtmccarthy@wpi.edu>
2019-10-29 20:54:44 -0400

    Description and initial tests and models

---
+++ python/375-card-flipping/card_flipper.py
@@ -0,0 +1,53 @@
+from typing import Dict, List, Union, Optional
+
+Card = bool
+
+Move = int
+
+Game = List[Optional[Card]]
+
+
+def is_face_up(card: Card) -> bool:
+    return card
+
+
+def can_flip(card: Card) -> bool:
+    return is_face_up(card)
+
+
+def make_move(game: Game, index: int) -> Optional[Game]:
+    # Flip card
+    # flip neighbors
+    return None
+
+def flip_neighbors(game: Optional[Game], index: int) -> Optional[Game]:
+    if game is None:
+        return None
+    return list(map(flip_neighbor(),
+                    list(map(lambda card: index, zip_game_with_index(index, game)))))
+
+
+def zip_game_with_index(index, game):
+    incremental_indexes = list(range(0, len(game)))
+    list_of_index = [index] * len(game)
+    return list(zip(list_of_index, incremental_indexes, game))
+
+
+
+def should_flip_neighbor(flipped_index, neighbor_index) -> bool:
+    pass
+
+def flip_neighbor(flipped_index, neighbor_index, neighbor_value) -> Optional[Card]:
+    pass
+
+
+def flip_card(game: Game, index: int) -> Optional[Game]:
+    if index < 0:
+        print("Invalid Index! Must be greater than 0.")
+        return None
+    elif index > len(game) - 1:
+        print("Invalid Index! Must be less than the size of the game - 1")
+        return None
+
+
+
---
+++ python/375-card-flipping/card_flipper_tests.py
@@ -0,0 +1,72 @@
+import unittest
+from card_flipper import *
+
+
+class TestCardFlipper(unittest.TestCase):
+
+    def setUp(self):
+        self.face_up_card = Card(True)
+        self.face_down_card = Card(False)
+
+        self.example_game = [
+            self.face_down_card,
+            self.face_up_card,
+            self.face_down_card,
+            self.face_down_card,
+            self.face_up_card,
+            self.face_up_card,
+            self.face_down_card
+        ]
+
+        self.example_game_flipped_at_one = [
+            self.face_down_card,
+            None,
+            self.face_down_card,
+            self.face_down_card,
+            self.face_up_card,
+            self.face_up_card,
+            self.face_down_card
+        ]
+
+        self.example_game_flipped_at_one_flipped_neighbors = [
+            self.face_up_card,
+            None,
+            self.face_up_card,
+            self.face_down_card,
+            self.face_up_card,
+            self.face_up_card,
+            self.face_down_card
+        ]
+
+    def test_is_face_up(self):
+        self.assertTrue(is_face_up(self.face_up_card))
+        self.assertFalse(is_face_up(self.face_down_card))
+
+    def test_flip_card(self):
+        self.assertEquals(None, flip_card([], -1))
+        self.assertEquals(None, flip_card([], 1))
+        self.assertEquals(self.example_game_flipped_at_one,
+                          flip_card(self.example_game, 1))
+        self.assertEquals(self.example_game, flip_card(self.example_game, 0))
+
+    def test_flip_neighbors(self):
+        self.assertEquals(self.example_game_flipped_at_one_flipped_neighbors,
+                          flip_neighbors(self.example_game_flipped_at_one, 1)
+                          )
+        self.assertEquals([None, True, None], flip_neighbors([None, False, None], 1))
+        self.assertEquals([None, False, True], flip_neighbors([None, False, False]))
+        self.assertEquals(None, flip_neighbors(None, 0))
+
+    def test_make_move(self):
+        self.assertEquals(self.example_game_flipped_at_one_flipped_neighbors,
+                          make_move(self.example_game, 1))
+        self.assertEquals(None, make_move([False, True, False], 0))
+
+    def test_zip_game_with_index(self):
+        game = [True, None, False]
+        index = 1
+        self.assertEquals(
+            [(1, 0, True), (1, 1, None), (1, 2, False)],
+            zip_game_with_index(index, game)
+        )
+
