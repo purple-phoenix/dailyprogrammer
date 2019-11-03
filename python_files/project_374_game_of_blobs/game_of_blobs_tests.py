dd15ef046c3c6812b600b7023e879fe27ed8d496
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-03 15:51:14 -0500

    Stubs for game of blobs

---
+++ python_files/project_374_game_of_blobs/game_of_blobs_tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestGameOfBlobs(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_foo(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
