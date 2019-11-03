fc15af6a389c98e51a831c881284769291964d0b
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-03 16:33:06 -0500

    Add high level test cases and outline structure

---
+++ python_files/project_374_game_of_blobs/game_of_blobs.py
@@ -0,0 +1,29 @@
+from typing import Tuple, List
+
+# Blobs are represented as a x position, y position and size
+# Positions are zero indexed, Blobs of size zero represented empty spaces
+Blob = Tuple[int, int, int]
+
+
+# One tick of the game, blobs move, then
+def blob_tick(blobs: List[Blob]) -> List[Blob]:
+    unmerged_blobs = move_blobs(blobs)
+    merged_blobs = merge_blobs(unmerged_blobs)
+    return merged_blobs
+
+
+def move_blobs(blobs: List[Blob]) -> List[Blob]:
+    pass
+
+
+def merge_blobs(blobs: List[Blob]) -> List[Blob]:
+    return merge_blobs(blobs, [])
+
+
+def merge_blobs_helper(blobs: List[Blob], observed_blobs: List[Blob]) -> List[Blob]:
+    if not blobs:
+        return observed_blobs
+    else:
+        blob = blobs[0]
+        pass
+
--- python_files/project_374_game_of_blobs/game_of_blobs_tests.py
+++ python_files/project_374_game_of_blobs/game_of_blobs_tests.py
@@ -1,13 +1,29 @@
 import unittest
+from project_374_game_of_blobs.game_of_blobs import *
 
 
 class TestGameOfBlobs(unittest.TestCase):
 
-    def setUp(self) -> None:
-        pass
+    def test_blob_tick(self):
+        initial_blobs = [(0, 2, 1), (2, 1, 2)]
+        one_tick = [(0, 2, 1), (1, 1, 2)]
+        two_tick = [(0, 2, 3)]
+        self.assertEqual(one_tick, blob_tick(initial_blobs))
+        self.assertEqual(two_tick, blob_tick(one_tick))
 
-    def test_foo(self):
-        pass
+    def test_move_blob(self):
+        initial_blobs = [(0, 2, 1), (2, 1, 2)]
+        one_tick = [(0, 2, 1), (1, 1, 2)]
+        two_tick = [(0, 2, 1), (0, 2, 2)]
+        self.assertEqual(one_tick, move_blobs(initial_blobs))
+        self.assertEqual(two_tick, move_blobs(one_tick))
+
+    def test_merge_blobs(self):
+        initial_blobs = [(0, 2, 1), (2, 1, 2)]
+        one_tick = [(0, 2, 1), (1, 1, 2)]
+        two_tick = [(0, 2, 3)]
+        self.assertEqual(one_tick, merge_blobs(initial_blobs))
+        self.assertEqual(two_tick, merge_blobs(one_tick))
 
 
 if __name__ == '__main__':
