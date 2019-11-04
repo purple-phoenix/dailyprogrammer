088497bd3a3595bbd5c667c09444b860f848bcec
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-04 18:20:28 -0500

    Impl convert_input_str_to_dice_input

--- python_files/project_364_dice_roller/dice_roller.py
+++ python_files/project_364_dice_roller/dice_roller.py
@@ -13,4 +13,10 @@ def roll_dice(dice_input: DiceInput) -> List[int]:
 
 
 def convert_input_str_to_dice_input(input_str: List[str]) -> DiceInput:
-    pass
+    return list(map(lambda a_input_str: split_str_by_to_int(a_input_str, 'd'), input_str))
+
+
+# Splits by first instance of splitter
+def split_str_by_to_int(input_str: str, splitting_char: str) -> Tuple[str, str]:
+    split_str = input_str.split(splitting_char)
+    return int(split_str[0]), int("".join(split_str[1:]))
