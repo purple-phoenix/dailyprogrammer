b4cbeee2b5b6f461225e41596e00047f8a829432
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-04 18:12:47 -0500

    Tests and stubs

---
+++ python_files/project_364_dice_roller/dice_roller.py
@@ -0,0 +1,16 @@
+from typing import Tuple, List
+
+
+# Number of die and number of sides per die
+DiceRoll = Tuple[int, int]
+
+
+DiceInput = List[DiceRoll]
+
+
+def roll_dice(dice_input: DiceInput) -> List[int]:
+    pass
+
+
+def convert_input_str_to_dice_input(input_str: List[str]) -> DiceInput:
+    pass
