58c329c291e9beb65e3b4ec826c0ed39ae8c45c2
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-05 13:55:28 -0500

    Implement make_clock_statement at high level with helper clock_num_to_str

--- python_files/project_321_talking_clock/talking_clock.py
+++ python_files/project_321_talking_clock/talking_clock.py
@@ -10,4 +10,25 @@ def make_clock_time(clock_str: str) -> ClockTime:
 
 
 def make_clock_statement(clock_time: ClockTime) -> str:
+    num_tens_hours = clock_time[0]
+    num_ones_hours = clock_time[1]
+    num_tens_minutes = clock_time[2]
+    num_one_minutes = clock_time[3]
+
+    num_hours = 10 * num_tens_hours + num_ones_hours
+
+    if num_hours > 12:
+        num_hours_with_am_pm = num_tens_hours - 12
+        return "It's" + \
+               clock_num_to_str(num_hours_with_am_pm) + \
+               clock_num_to_str(num_tens_minutes) + \
+               clock_num_to_str(num_one_minutes) + "pm"
+    else:
+        num_hours_with_am_pm = num_tens_hours
+        return "It's" + clock_num_to_str(num_hours_with_am_pm) + \
+               clock_num_to_str(num_tens_minutes) + \
+               clock_num_to_str(num_one_minutes) + "am"
+
+
+def clock_num_to_str(clock_num: int) -> str:
     pass
