e39afabae572b5c36f2d7d638c87a47aaa776c56
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-04 21:15:31 -0500

    Implement get num_each_char

--- python_files/project_372_perfectly_balanced/perfectly_balanced.py
+++ python_files/project_372_perfectly_balanced/perfectly_balanced.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 
 def balanced(string: str) -> bool:
@@ -10,4 +10,23 @@ def balanced_bonus(string: str) -> bool:
 
 
 def num_each_char(string: str) -> Dict[str, int]:
-    pass
+    return num_each_char_helper([char for char in string],  {})
+
+
+def num_each_char_helper(char_stream: List[str], accum: Dict[str, int]) -> Dict:
+    if not char_stream:
+        return accum
+    else:
+        this_char = char_stream[0]
+        rest_chars = char_stream[1:]
+        if this_char in accum:
+            # Not pure functional but I think this is fine
+            # it's about knowing when to break the rules
+            current_count = accum[this_char]
+            accum[this_char] = current_count + 1
+            return num_each_char_helper(rest_chars, accum)
+        else:
+            # First instance of this character
+            accum[this_char] = 1
+            return num_each_char_helper(rest_chars, accum)
+
