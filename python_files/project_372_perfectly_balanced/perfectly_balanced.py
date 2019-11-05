7afc016844c1be10d03a2d6de70d52d2c3d06cd6
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-04 21:00:22 -0500

    Add tests

---
+++ python_files/project_372_perfectly_balanced/perfectly_balanced.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+
+
+def balanced(string: str) -> bool:
+    pass
+
+
+def balanced_bonus(string: str) -> bool:
+    pass
--- python_files/project_372_perfectly_balanced/perfectly_balanced_tests.py
+++ python_files/project_372_perfectly_balanced/perfectly_balanced_tests.py
@@ -1,9 +1,27 @@
 import unittest
+from project_372_perfectly_balanced.perfectly_balanced import *
 
 
 class TestPerfectlyBalanced(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_balanced(self):
+        self.assertTrue(balanced("xxxyyy"))
+        self.assertTrue(balanced("yyyxxx"))
+        self.assertFalse(balanced("xxxyyyy"))
+        self.assertTrue(balanced("yyxyxxyxxyyyyxxxyxyx"))
+        self.assertFalse(balanced("xyxxxxyyyxyxxyxxyy"))
+        self.assertTrue(balanced(""))
+        self.assertFalse(balanced("x"))
+
+    def test_balanced_bonus(self):
+        self.assertTrue(balanced_bonus("xxxyyyzzz"))
+        self.assertTrue(balanced_bonus("abccbaabccba"))
+        self.assertFalse(balanced_bonus("xxxyyyzzzz"))
+        self.assertTrue(balanced_bonus("abcdefghijklmnopqrstuvwxyz"))
+        self.assertFalse(balanced_bonus("pqq"))
+        self.assertFalse(balanced_bonus("fdedfdeffeddefeeeefddf"))
+        self.assertTrue(balanced_bonus("www"))
+        self.assertTrue(balanced_bonus("x"))
+        self.assertTrue(balanced_bonus(""))
 
 
 if __name__ == '__main__':
