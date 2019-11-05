3ec2ddd4a65b46636e271deedac55144b90e47a8
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-05 14:24:10 -0500

    Break logic out into a few functions, also fix the tests to pass tuple

--- python_files/project_321_talking_clock/talking_clock.py
+++ python_files/project_321_talking_clock/talking_clock.py
@@ -16,19 +16,59 @@ def make_clock_statement(clock_time: ClockTime) -> str:
     num_one_minutes = clock_time[3]
 
     num_hours = 10 * num_tens_hours + num_ones_hours
-
     if num_hours > 12:
-        num_hours_with_am_pm = num_tens_hours - 12
-        return "It's" + \
-               clock_num_to_str(num_hours_with_am_pm) + \
-               clock_num_to_str(num_tens_minutes) + \
-               clock_num_to_str(num_one_minutes) + "pm"
+        num_hours_to_print = num_hours - 12
+        hours_str = make_hours_str(num_hours_to_print)
+        minutes_str = make_minutes_str(num_tens_minutes, num_one_minutes)
+        return make_full_output(hours_str, minutes_str, "pm")
     else:
-        num_hours_with_am_pm = num_tens_hours
-        return "It's" + clock_num_to_str(num_hours_with_am_pm) + \
-               clock_num_to_str(num_tens_minutes) + \
-               clock_num_to_str(num_one_minutes) + "am"
+        num_hours_to_print = num_hours
+        hours_str = make_hours_str(num_hours_to_print)
+        minutes_str = make_minutes_str(num_tens_minutes, num_one_minutes)
+        return make_full_output(hours_str, minutes_str, "am")
 
 
-def clock_num_to_str(clock_num: int) -> str:
-    pass
+def make_full_output(hours_str: str, minutes_str: str, am_pm: str) -> str:
+    return ""
+
+
+def make_hours_str(num_hours: str) -> str:
+    return ""
+
+
+def make_minutes_str(num_ten_minutes: str, num_one_minutes: str) -> str:
+    return ""
+
+
+def clock_num_to_str(clock_num: int, is_hours: bool = False) -> str:
+    zero_rep = ""
+    if is_hours:
+        zero_rep = "twelve"
+    else:
+        zero_rep = "oh"
+    return " " + {
+        0: zero_rep,
+        1: "one",
+        2: "two",
+        3: "three",
+        5: "five",
+        4: "four",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "fourty",
+        50: "fifty"
+    }[clock_num] + " "
