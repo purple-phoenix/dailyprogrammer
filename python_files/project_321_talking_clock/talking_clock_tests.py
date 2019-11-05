dd342e6321ecd148c4d31e368c5b94ff42beceac
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-05 13:29:25 -0500

    Stubs and readme

---
+++ python_files/project_321_talking_clock/talking_clock_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from project_321_talking_clock.talking_clock import *
+
+
+class TestTalkingClock(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
