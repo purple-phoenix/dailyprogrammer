bb6f597d8d72892f8b16e9817847484a209cb936
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-05 23:51:34 -0500

    Readme and stubs

---
+++ python_files/project_239_game_of_threes/game_of_threes_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from project_239_game_of_threes.game_of_threes import *
+
+
+class TestGameOfThrees(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
