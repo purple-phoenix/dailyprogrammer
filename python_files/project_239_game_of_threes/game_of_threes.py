80043fa2f021d4d2b42b217bd6bd519f05f39a1b
Alex Showers <ajshower@gmail.com>
2019-11-09 18:19:11 -0500

    Collection of suggested best practices:   Do not name variables "input"   Return False for invalid inputs   Check for invalid inputs first   Start private functions with an underscore

--- python_files/project_239_game_of_threes/game_of_threes.py
+++ python_files/project_239_game_of_threes/game_of_threes.py
@@ -1,30 +1,42 @@
+##
+# Do not name variables "input" as input is an existing variable in python:
+#  https://stackoverflow.com/questions/20670732/is-input-a-keyword-in-python
+#
+# By convention, internal functions should start with an underscore.
+# https://stackoverflow.com/questions/11483366/protected-method-in-python
+##
 
-
-def game_of_threes(input: int) -> ():
-    if input == 1:
-        print(input)
-        return
-    elif divisible_by_three(input):
-        print(str(input) + " 0")
-        return game_of_threes(int(input/3))
-    elif add_one_divisible_by_three(input):
-        print(str(input) + " 1")
-        return game_of_threes(input + 1)
-    elif sub_one_divisible_by_three(input):
-        print(str(input) + " -1")
-        return game_of_threes(input - 1)
-    else:
+##
+#  @param  operand:  number to operate on.
+#  @return boolean:  If inputs are valid.
+##
+def game_of_threes(operand: int) -> bool:
+    # Check for invalid inputs first
+    if operand < 1:
         print("Invalid input. Starting number must be greater than zero")
-        return
+        return False
+    if operand == 1:
+        print(operand)
+        return True
+    # As prior functional blocks all return, there is no need for "elif" control blocks
+    if _divisible_by_three(operand):
+        print(str(operand) + " 0")
+        return game_of_threes(int(operand / 3))
+    if _add_one_divisible_by_three(operand):
+        print(str(operand) + " 1")
+        return game_of_threes(operand + 1)
+    if _sub_one_divisible_by_three(operand):
+        print(str(operand) + " -1")
+        return game_of_threes(operand - 1)
 
 
-def divisible_by_three(num: int) -> bool:
+def _divisible_by_three(num: int) -> bool:
     return num % 3 == 0
 
 
-def add_one_divisible_by_three(num: int) -> bool:
+def _add_one_divisible_by_three(num: int) -> bool:
     return (num + 1) % 3 == 0
 
 
-def sub_one_divisible_by_three(num: int) -> bool:
+def _sub_one_divisible_by_three(num: int) -> bool:
     return (num - 1) % 3 == 0
