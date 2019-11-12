daba5fcf2816e8af3f2b1b8ac5455fd3dff75256
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-12 07:40:23 -0500

    Yahtzee upper tests

--- rust_files/src/project_381_yahtzee/yahtzee.rs
+++ rust_files/src/project_381_yahtzee/yahtzee.rs
@@ -1,9 +1,18 @@
 
+fn yahtzee_upper(five_dice_roll: Vec<usize>) -> usize {
+    return 0;
+}
+
 
 #[cfg(test)]
 mod tests {
+    use crate::project_381_yahtzee::yahtzee::yahtzee_upper;
 
-    fn test_foo() {
-
+    fn test_yahtzee_upper() {
+        assert_eq!(yahtzee_upper(vec![2, 3, 5, 5, 6]), 10);
+        assert_eq!(yahtzee_upper(vec![1, 1, 1, 1, 3]), 4);
+        assert_eq!(yahtzee_upper(vec![1, 1, 1, 3, 3]), 6);
+        assert_eq!(yahtzee_upper(vec![1, 2, 3, 4, 5]), 5);
+        assert_eq!(yahtzee_upper(vec![6, 6, 6, 6, 6]), 30);
     }
 }
