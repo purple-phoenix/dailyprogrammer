27e250e2dcb66abc059e1efcbda822a35fba7279
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-12 14:17:58 -0500

    Helper functions for copying graphs and maps as well as tests for sub graphs

--- rust_files/src/project_375_graph_of_thrones/graph_of_thrones.rs
+++ rust_files/src/project_375_graph_of_thrones/graph_of_thrones.rs
@@ -54,13 +54,58 @@ impl <T> UndirectedCompleteGraph<T> where T: Eq + Hash + PartialOrd + Copy {
         return self.num_edges;
     }
 
+    pub fn get_graph(&self) -> HashMap<T, Vec<(T, bool)>> {
+        return copy_map(&self.graph);
+    }
+
+    pub fn copy_graph(&self) -> UndirectedCompleteGraph<T> {
+        return UndirectedCompleteGraph {
+            num_edges: self.num_edges,
+            num_nodes: self.num_nodes,
+            graph: copy_map(&self.graph)
+        }
+    }
+
     fn is_simple_graph(&self) -> bool {
         return self.num_nodes == 3;
     }
 
     fn make_sub_graphs(&self) -> Vec<UndirectedCompleteGraph<T>> {
+        if self.is_simple_graph() {
+            return vec![self.copy_graph()]
+        }
         return vec![]
     }
+}
+
+
+fn maps_are_equal<T, E>(map1: &HashMap<T, E>, map2: &HashMap<T, E>) -> bool
+    where T: Eq + Hash, E: Eq
+{
+    if map1.len() != map2.len() {
+        return false;
+    }
+    else {
+        let mut all_key_vals_are_equal = true;
+        for (key, val) in map1 {
+            if !map2.contains_key(key) {
+                return false;
+            }
+            else {
+                let map2_value = map2.get(key).unwrap();
+                all_key_vals_are_equal &= val == map2_value
+            }
+        }
+        return all_key_vals_are_equal;
+    }
+}
+
+fn copy_map<T, E>(map: &HashMap<T, E>) -> HashMap<T, E> where T: Hash + Clone + Eq, E: Clone + Eq {
+    let mut new_map = HashMap::new();
+    for (key, value) in map {
+        new_map.insert(key.clone(), value.clone());
+    }
+    return new_map;
 }
 
 
@@ -68,7 +113,7 @@ impl <T> UndirectedCompleteGraph<T> where T: Eq + Hash + PartialOrd + Copy {
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
-    use crate::project_375_graph_of_thrones::graph_of_thrones::UndirectedCompleteGraph;
+    use crate::project_375_graph_of_thrones::graph_of_thrones::{UndirectedCompleteGraph, maps_are_equal};
 
     #[test]
     fn test_make_graph() {
@@ -139,6 +184,43 @@ mod tests {
             UndirectedCompleteGraph::make_graph(&unbalanced_graph);
 
         assert!(!unbalanced_graph_struct.is_simple_graph());
+
+    }
+
+
+
+    #[test]
+    fn test_make_sub_graphs() {
+        let node1 = "Node1";
+        let node2 = "Node2";
+        let node3 = "Node3";
+        let node4 = "Node4";
+
+        let mut simple_graph = HashMap::new();
+        simple_graph.insert(node1, vec![(node2, true), (node3, false)]);
+        simple_graph.insert(node2, vec![(node1, true), (node3, false)]);
+        simple_graph.insert(node3, vec![(node1, false), (node2, false)]);
+        let actual_graph =
+            UndirectedCompleteGraph::make_graph(&simple_graph);
+
+        let sub_graphs = actual_graph.make_sub_graphs();
+        assert_eq!(sub_graphs.len(), 1);
+        let before_graph = actual_graph.get_graph();
+        let sub_graph = sub_graphs.get(0).unwrap().get_graph();
+        println!("{:?}...{:?}", before_graph, sub_graph);
+        assert!(maps_are_equal(&before_graph, &sub_graph));
+
+
+
+        let mut unbalanced_graph = HashMap::new();
+        unbalanced_graph.insert(node1, vec![(node2, false), (node3, false), (node4, false)]);
+        unbalanced_graph.insert(node2, vec![(node3, true), (node4, true)]);
+        unbalanced_graph.insert(node3, vec![(node4, false)]);
+        unbalanced_graph.insert(node4, vec![]);
+        let unbalanced_graph_struct =
+            UndirectedCompleteGraph::make_graph(&unbalanced_graph);
+
+        let sub_graphs = unbalanced_graph_struct.make_sub_graphs();
 
     }
 
