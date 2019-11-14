d00379ea1bbb1563569097f1d99e268f5773797e
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-13 20:53:30 -0500

    Implement insert

--- rust_files/src/utility/AVLTree.rs
+++ rust_files/src/utility/AVLTree.rs
@@ -8,7 +8,9 @@ pub struct AVLTree<T> {
 
 }
 
-impl <T> AVLTree<T> {
+impl <T> AVLTree<T>
+where T: PartialOrd
+{
 
     pub fn make_empty_tree(data: T) -> AVLTree<T> {
         return AVLTree {
@@ -17,6 +19,49 @@ impl <T> AVLTree<T> {
             right_child: None,
             height: 0
         };
+    }
+
+    pub fn get_height(self) -> isize {
+        return self.height;
+    }
+
+    pub fn insert(self, new_node_value: T) -> AVLTree<T> {
+        if new_node_value > self.data {
+            match self.right_child {
+                None => return AVLTree {
+                    data: self.data,
+                    left_child: self.left_child,
+                    right_child: Some(Box::new(
+                        AVLTree::make_empty_tree(new_node_value))),
+                    height: self.height + 1
+                },
+                Some(right_child) => return AVLTree {
+                        data: self.data,
+                        left_child: self.left_child,
+                        right_child: Some(Box::new(right_child.insert(new_node_value))),
+                        height: self.height + 1
+                }
+            }
+        }
+        else {
+            match self.left_child {
+                None => return AVLTree {
+                    data: self.data,
+                    left_child: Some(Box::new(
+                        AVLTree::make_empty_tree(new_node_value))),
+                    right_child: self.right_child,
+                    height: self.height + 1
+                },
+                Some(left_child) => {
+                    return AVLTree {
+                        data: self.data,
+                        left_child: Some(Box::new(left_child.insert(new_node_value))),
+                        right_child: self.right_child,
+                        height: self.height + 1
+                    }
+                }
+            }
+        }
     }
 
 }
@@ -37,6 +82,36 @@ mod test {
             right_child: None,
             height: 0
         });
+    }
+
+    #[test]
+    fn test_get_height() {
+        let empty_tree = AVLTree::make_empty_tree(34);
+        assert_eq!(empty_tree.get_height(), 0);
+    }
+
+    #[test]
+    fn test_insert() {
+        let base_tree = AVLTree::make_empty_tree(10);
+        let insert_to_right = base_tree.insert(15);
+        assert_eq!(insert_to_right, AVLTree {
+            data: 10,
+            left_child: None,
+            right_child: Some(Box::new(AVLTree::make_empty_tree(15))),
+            height: 1
+        });
+        let insert_left_then_right = insert_to_right.insert(13);
+        assert_eq!(insert_left_then_right, AVLTree {
+            data: 10,
+            left_child: None,
+            right_child: Some(Box::new(AVLTree {
+                data: 15,
+                left_child: Some(Box::new(AVLTree::make_empty_tree(13))),
+                right_child: None,
+                height: 1
+            })),
+            height: 2
+        })
     }
 
 }
