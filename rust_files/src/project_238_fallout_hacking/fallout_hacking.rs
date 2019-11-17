8e8c3570466abe45bd517962230823a95abfbc3a
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-16 20:19:46 -0500

    Move constant generation to its own module and add num words for each difficulty

--- rust_files/src/project_238_fallout_hacking/fallout_hacking.rs
+++ rust_files/src/project_238_fallout_hacking/fallout_hacking.rs
@@ -1,25 +1,4 @@
-extern crate rand;
-
-use self::rand::Rng;
-
-
-//Constants
-const MIN_VERY_EASY_WL :usize = 4;//[4]
-const MAX_VERY_EASY_WL :usize = 5;
-
-const MIN_EASY_WL :usize = 5;//[5,6,7]
-const MAX_EASY_WL :usize = 8;
-
-const MIN_AVERAGE_WL :usize = 8;//[8,9]
-const MAX_AVERAGE_WL :usize = 10;
-
-const MIN_HARD_WL :usize = 10;//[10,11,12]
-const MAX_HARD_WL :usize = 13;
-
-const MIN_VERY_HARD_WL:usize = 13;//[13,14,15]
-const MAX_VERY_HARD_WL:usize = 16;
-
-
+use crate::project_238_fallout_hacking::game_constants::*;
 
 struct FalloutHackingGame {
 
@@ -29,7 +8,7 @@ struct FalloutHackingGame {
     difficulty: GameDifficulty
 }
 
-enum GameDifficulty {
+pub enum GameDifficulty {
     VeryEasy,
     Easy,
     Average,
@@ -41,7 +20,7 @@ impl FalloutHackingGame {
 
     fn make_game(difficulty: GameDifficulty) -> FalloutHackingGame {
         FalloutHackingGame {
-            word_length:0,
+            word_length: make_word_length(&difficulty),
             other_words: vec![],
             correct_word: "".to_owned(),
             difficulty
@@ -50,16 +29,7 @@ impl FalloutHackingGame {
 
 }
 
-fn make_word_length(difficulty: GameDifficulty) -> usize {
-    let mut rng = rand::thread_rng();
-    return match difficulty {
-        GameDifficulty::VeryEasy => rng.gen_range(MIN_VERY_EASY_WL, MAX_VERY_EASY_WL),
-        GameDifficulty::Easy => rng.gen_range(MIN_EASY_WL, MAX_EASY_WL),
-        GameDifficulty::Average => rng.gen_range(MIN_AVERAGE_WL, MAX_AVERAGE_WL),
-        GameDifficulty::Hard => rng.gen_range(MIN_HARD_WL, MAX_HARD_WL),
-        GameDifficulty::VeryHard => rng.gen_range(MIN_VERY_HARD_WL, MAX_VERY_HARD_WL)
-    };
-}
+
 
 
 
@@ -74,28 +44,6 @@ mod tests {
 
 
         assert!(false);
-    }
-
-    #[test]
-    fn test_get_word_length() {
-        let very_easy_wl = make_word_length(GameDifficulty::VeryEasy);
-        let easy_wl = make_word_length(GameDifficulty::Easy);
-        let avg_wl = make_word_length(GameDifficulty::Average);
-        let hard_wl = make_word_length(GameDifficulty::Hard);
-        let very_hard_wl = make_word_length(GameDifficulty::VeryHard);
-
-        let very_easy_range = MIN_VERY_EASY_WL..MAX_VERY_EASY_WL;
-        let easy_range = MIN_EASY_WL..MAX_EASY_WL;
-        let avg_range = MIN_AVERAGE_WL..MAX_AVERAGE_WL;
-        let hard_range = MIN_HARD_WL..MAX_HARD_WL;
-        let very_hard_range = MIN_VERY_HARD_WL..MAX_VERY_HARD_WL;
-
-        assert!(very_easy_range.contains(&very_easy_wl));
-        assert!(easy_range.contains(&easy_wl));
-        assert!(avg_range.contains(&avg_wl));
-        assert!(hard_range.contains(&hard_wl));
-        assert!(very_hard_range.contains(&very_hard_wl));
-
     }
 
 }
--- rust_files/src/project_238_fallout_hacking/mod.rs
+++ rust_files/src/project_238_fallout_hacking/mod.rs
@@ -1,1 +1,2 @@
 pub mod fallout_hacking;
+mod game_constants;
