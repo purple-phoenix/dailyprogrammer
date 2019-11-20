fbf91dbfd3bdbadb6fbbefe73603040f7eb814f2
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-19 19:37:01 -0500

    Impl get largest 10s digit

--- rust_files/src/project_375_print_number_digit/print_number_digit.rs
+++ rust_files/src/project_375_print_number_digit/print_number_digit.rs
@@ -1,13 +1,65 @@
 
 
+fn increment_digits(num: i32) -> i32 {
+    return num;
+}
+
+fn get_digits(num: i32) -> Vec<usize> {
+    return vec![]
+}
+
+fn get_largest_digit_place(num: i32) -> usize {
+    if num == 0 {
+        return 1;
+    }
+    else if num > 0 {
+        let mut init_digit_place_holder = 1;
+        while num - init_digit_place_holder > 0 {
+            init_digit_place_holder *= 10;
+            println!("{}", init_digit_place_holder)
+        }
+        return (init_digit_place_holder / 10) as usize;
+    }
+    else {
+        //num is negative
+        let mut init_digit_place_holder = 1;
+        while num + init_digit_place_holder < 0 {
+            init_digit_place_holder *= 10;
+        }
+        return (init_digit_place_holder / 10) as usize;
+    }
+
+
+}
+
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
 
-    fn test_foo() {
+    #[test]
+    fn test_increment_digits() {
+        assert_eq!(increment_digits(998), 10109);
+        assert_eq!(increment_digits(0), 1);
 
+
+        //TODO
+        //assert_eq!(increment_digits(-998), -887);
+    }
+    #[test]
+    fn test_get_digits() {
+        assert_eq!(get_digits(998), vec![9, 9, 8]);
+        assert_eq!(get_digits(0), vec![0]);
+        //TODO
+        //assert_eq!(get_digits(-998), vec![-9, -9, -8])
+    }
+    #[test]
+    fn test_get_largest_digit_place() {
+        assert_eq!(get_largest_digit_place(998), 100);
+        assert_eq!(get_largest_digit_place(0), 1);
+        assert_eq!(get_largest_digit_place(7), 1);
+        assert_eq!(get_largest_digit_place(-998), 100)//?
     }
 
 }
